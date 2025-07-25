bd4614897c20e6f62da1a28dbc9bfd50924491fc
KyleNewman97 <kyle@thenewmans.com.au>
2025-07-25 13:21:51 +0800

    chore: added automated testing and linting (#1)

---
+++ src/ssdv2/structs/exceptions/__init__.py
@@ -0,0 +1,3 @@
+from .dataset_error import DatasetError
+
+__all__ = ("DatasetError",)
---
+++ src/ssdv2/structs/exceptions/dataset_error.py
@@ -0,0 +1,6 @@
+class DatasetError(Exception):
+    """
+    An error to be raised when the structure or the contents of a dataset is incorrect.
+    """
+
+    pass
