f958a9d1ee77d801ffb08f55af030a3a1679b22e
KyleNewman97 <kyle@thenewmans.com.au>
2025-08-06 05:17:48 +0000

    feat(ssd): linked up forward path

--- src/ssdv2/models/ssdv2.py
+++ src/ssdv2/models/ssdv2.py
@@ -1,4 +1,4 @@
-from torch import nn
+from torch import Tensor, nn
 
 from ssdv2.models.backbones import ConvNeXtBackbone
 from ssdv2.models.heads import FCOSHead
@@ -10,3 +10,8 @@ class SSDv2(nn.Module):
         self.backbone = ConvNeXtBackbone(depths=[3, 3, 3, 3], dims=[48, 96, 192, 384])
         self.neck = ConvNeXtNeck(depths=[0, 3, 3, 3], fm_channels=[384, 192, 96, 48])
         self.head = FCOSHead(num_classes, self.neck.out_fm_channels)
+
+    def forward(self, images: Tensor) -> tuple[Tensor, Tensor, Tensor]:
+        backbone_fms = self.backbone.forward(images)
+        neck_fms = self.neck.forward(backbone_fms)
+        return self.head.forward(neck_fms)
