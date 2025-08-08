a690b23d0a58810dae0402d82df0453c33492ee9
KyleNewman97 <kyle@thenewmans.com.au>
2025-08-08 05:55:00 +0000

    feat(transforms): added letterbox transform

--- src/ssdv2/models/ssdv2.py
+++ src/ssdv2/models/ssdv2.py
@@ -3,6 +3,7 @@ from torch import Tensor, nn
 from ssdv2.models.backbones import ConvNeXtBackbone
 from ssdv2.models.heads import FCOSHead
 from ssdv2.models.necks import ConvNeXtNeck
+from ssdv2.structs.configs import TrainConfig
 
 
 class SSDv2(nn.Module):
@@ -17,3 +18,50 @@ class SSDv2(nn.Module):
         backbone_fms = self.backbone.forward(images)
         neck_fms = self.neck.forward(backbone_fms)
         return self.head.forward(neck_fms)
+
+    def fit(self, config: TrainConfig):
+        pass
+
+    # def _create_data_loaders(
+    #     self, config: TrainConfig
+    # ) -> tuple[DataLoader, DataLoader]:
+    #     """
+    #     Creates the training and validation data loaders.
+    #     """
+    #     # Create the transform to use with all images being fed into the model
+    #     transform = LetterboxTransform(
+    #         config.image_width, config.image_height, config.dtype
+    #     )
+
+    #     # Create the collate function
+    #     collate_func = partial(TrainUtils.batch_collate_func, device=self.device)
+
+    #     # Create the training dataset loader
+    #     train_dataset = SSDDataset(
+    #         config.train_images_dir,
+    #         config.train_labels_dir,
+    #         config.num_classes,
+    #         transform,
+    #         None,
+    #         self.device,
+    #         config.dtype,
+    #     )
+    #     train_loader = DataLoader(
+    #         train_dataset, config.batch_size, shuffle=True, collate_fn=collate_func
+    #     )
+
+    #     # Create the validation dataset loader
+    #     val_dataset = SSDDataset(
+    #         config.val_images_dir,
+    #         config.val_labels_dir,
+    #         config.num_classes,
+    #         transform,
+    #         None,
+    #         self.device,
+    #         config.dtype,
+    #     )
+    #     val_loader = DataLoader(
+    #         val_dataset, config.batch_size, shuffle=False, collate_fn=collate_func
+    #     )
+
+    #     return train_loader, val_loader
